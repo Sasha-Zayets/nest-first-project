112172ed25f352a6c4f2f0c0145723d9e7e579e3
Sasha-Zayets <sachazaest@ukr.net>
2022-11-02 23:32:39 +0200

    Added base validation for API

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,4 +1,13 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 export class SignupDto {
+  @IsEmail({
+    message: 'Email is not correct',
+  })
   email: string;
+
+  @IsNotEmpty()
+  @MinLength(6, {
+    message: 'Password is too short',
+  })
   password: string;
 }
--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,5 +1,13 @@
+import { IsNotEmpty, MinLength, IsBoolean } from 'class-validator';
 export class CreateTaskDto {
+  @IsNotEmpty()
+  @MinLength(2, {
+    message: 'Title is too short, Minimal length $constraint1',
+  })
   title: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
   done: boolean;
 }
 
