638b56e6a0ed1c59fd29b2e3b9370a770c4d765b
Sasha-Zayets <sachazaest@ukr.net>
2022-11-26 17:48:50 +0200

    Change websocket server and added cors for application

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -6,13 +6,18 @@ import {
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
 export class ChatGateway {
   @WebSocketServer()
   server: Server;
 
   @SubscribeMessage('send_message')
   handleSendMessage(@MessageBody() data: string): string {
+    console.log(data);
     return 'Hello world!';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
@@ -17,6 +17,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
